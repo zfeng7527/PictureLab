5ff04dd92d44a7b3aa4ed7eaebed49ba495b6473
BT_1N3_17 <itsbenwu>
2019-03-22 08:26:10 -0400

    Act 4 Dun

--- classes/IntArrayWorker.java
+++ classes/IntArrayWorker.java
@@ -1,94 +1,83 @@
-public class IntArrayWorker
-{
-  /** two dimensional matrix */
+public class IntArrayWorker {
+  /**
+   * two dimensional matrix
+   */
   private int[][] matrix = null;
-  
-  /** set the matrix to the passed one
-    * @param theMatrix the one to use
-    */
-  public void setMatrix(int[][] theMatrix)
-  {
+
+  /**
+   * set the matrix to the passed one
+   *
+   * @param theMatrix the one to use
+   */
+  public void setMatrix(int[][] theMatrix) {
     matrix = theMatrix;
   }
-  
+
   /**
-   * Method to return the total 
+   * Method to return the total
+   *
    * @return the total of the values in the array
    */
-  public int getTotal()
-  {
+  public int getTotal() {
     int total = 0;
-    for (int row = 0; row < matrix.length; row++)
-    {
-      for (int col = 0; col < matrix[0].length; col++)
-      {
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[0].length; col++) {
         total = total + matrix[row][col];
       }
     }
     return total;
   }
-  
+
   /**
    * Method to return the total using a nested for-each loop
+   *
    * @return the total of the values in the array
    */
-  public int getTotalNested()
-  {
+  public int getTotalNested() {
     int total = 0;
-    for (int[] rowArray : matrix)
-    {
-      for (int item : rowArray)
-      {
+    for (int[] rowArray : matrix) {
+      for (int item : rowArray) {
         total = total + item;
       }
     }
     return total;
   }
-  
+
   /**
    * Method to fill with an increasing count
    */
-  public void fillCount()
-  {
+  public void fillCount() {
     int numCols = matrix[0].length;
     int count = 1;
-    for (int row = 0; row < matrix.length; row++)
-    {
-      for (int col = 0; col < numCols; col++)
-      {
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < numCols; col++) {
         matrix[row][col] = count;
         count++;
       }
     }
   }
-  
+
   /**
    * print the values in the array in rows and columns
    */
-  public void print()
-  {
-    for (int row = 0; row < matrix.length; row++)
-    {
-      for (int col = 0; col < matrix[0].length; col++)
-      {
-        System.out.print( matrix[row][col] + " " );
+  public void print() {
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[0].length; col++) {
+        System.out.print(matrix[row][col] + " ");
       }
       System.out.println();
     }
     System.out.println();
   }
-  
-  
-  /** 
+
+
+  /**
    * fill the array with a pattern
    */
-  public void fillPattern1()
-  {
-    for (int row = 0; row < matrix.length; row++)
-    {
-      for (int col = 0; col < matrix[0].length; 
-           col++)
-      {
+  public void fillPattern1() {
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[0].length;
+           col++) {
         if (row < col)
           matrix[row][col] = 1;
         else if (row == col)
@@ -99,16 +88,35 @@ public class IntArrayWorker
     }
   }
 
-  public int getCount(int num){
+  public int getCount(int num) {
     int count = 0;
-      for(int x=0;x<matrix.length;x++){
-        for(int y=0;y<matrix[x].length;y++){
-          if(matrix[x][y]==num){
-            count++;
-          }
+    for (int x = 0; x < matrix.length; x++) {
+      for (int y = 0; y < matrix[x].length; y++) {
+        if (matrix[x][y] == num) {
+          count++;
         }
       }
+    }
     return count;
-   }
- 
+  }
+
+  public int getLargest() {
+    int largest = 0;
+    for (int x = 0; x < matrix.length; x++) {
+      for (int y = 0; y < matrix[x].length; y++) {
+        if (matrix[x][y] > largest) {
+          largest = matrix[x][y];
+        }
+      }
+    }
+    return largest;
+  }
+
+  public int getColTotal(int col){
+    int total = 0;
+    for(int i =0; i<matrix.length;i++){
+      total += matrix[i][col];
+    }
+    return total;
+  }
 }
--- classes/IntArrayWorkerTester.java
+++ classes/IntArrayWorkerTester.java
@@ -53,43 +53,43 @@ public class IntArrayWorkerTester
   }
   
   /** Method to test getLargest */
-//  public static void testGetLargest()
-//  { // test when largest is last
-//    IntArrayWorker worker = new IntArrayWorker();
-//    int [][] nums2 = {{1, 2, 3}, {4, 5, 6}};
-//    worker.setMatrix(nums2);
-//    int largest = worker.getLargest();
-//    System.out.println("Largest should be 6 and is " + largest); 
-//    // test when largest is first
-//    int[][] nums3 = {{6, 2, 3}, {4, 5, 1}};
-//    worker.setMatrix(nums3);
-//    largest = worker.getLargest();
-//    System.out.println("Largest should be 6 and is " + largest); 
-//    // test when largest is in the middle
-//    int[][] nums4 = {{1, 2, 3}, {6, 5, 1}};
-//    worker.setMatrix(nums4);
-//    largest = worker.getLargest();
-//    System.out.println("Largest should be 6 and is " + largest);
-//    // test when duplicate largest
-//    int[][] nums5 = {{6, 2, 6}, {4, 5, 1}};
-//    worker.setMatrix(nums5);
-//    largest = worker.getLargest();
-//    System.out.println("Largest should be 6 and is " + largest);
-//  }
+  public static void testGetLargest()
+  { // test when largest is last
+    IntArrayWorker worker = new IntArrayWorker();
+    int [][] nums2 = {{1, 2, 3}, {4, 5, 6}};
+    worker.setMatrix(nums2);
+    int largest = worker.getLargest();
+    System.out.println("Largest should be 6 and is " + largest);
+    // test when largest is first
+    int[][] nums3 = {{6, 2, 3}, {4, 5, 1}};
+    worker.setMatrix(nums3);
+    largest = worker.getLargest();
+    System.out.println("Largest should be 6 and is " + largest);
+    // test when largest is in the middle
+    int[][] nums4 = {{1, 2, 3}, {6, 5, 1}};
+    worker.setMatrix(nums4);
+    largest = worker.getLargest();
+    System.out.println("Largest should be 6 and is " + largest);
+    // test when duplicate largest
+    int[][] nums5 = {{6, 2, 6}, {4, 5, 1}};
+    worker.setMatrix(nums5);
+    largest = worker.getLargest();
+    System.out.println("Largest should be 6 and is " + largest);
+  }
   
   /** Method to test getColTotal */
-//  public static void testGetColTotal()
-//  {
-//    IntArrayWorker worker = new IntArrayWorker();
-//    int [][] nums2 = {{1, 2, 3}, {4, 5, 6}};
-//    worker.setMatrix(nums2);
-//    int total = worker.getColTotal(0);
-//    System.out.println("Total for column 0 should be 5 and is " + total);
-//    total = worker.getColTotal(1);
-//    System.out.println("Total for column 1 should be 7 and is " + total);
-//    total = worker.getColTotal(2);
-//    System.out.println("Total for column 2 should be 9 and is " + total);
-//  }
+  public static void testGetColTotal()
+  {
+    IntArrayWorker worker = new IntArrayWorker();
+    int [][] nums2 = {{1, 2, 3}, {4, 5, 6}};
+    worker.setMatrix(nums2);
+    int total = worker.getColTotal(0);
+    System.out.println("Total for column 0 should be 5 and is " + total);
+    total = worker.getColTotal(1);
+    System.out.println("Total for column 1 should be 7 and is " + total);
+    total = worker.getColTotal(2);
+    System.out.println("Total for column 2 should be 9 and is " + total);
+  }
   
   public static void main(String[] args)
   {
@@ -98,7 +98,7 @@ public class IntArrayWorkerTester
     testGetCount();
     testGetTotal();
     testGetTotalNested();
-    //testGetLargest();
-    //testGetColTotal();
+    testGetLargest();
+    testGetColTotal();
   }
 }
